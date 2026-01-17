7b68ffc302a5b5d94f6144254a3797da1c31c52f
Kazi Mahibol Hasan <33973244+itskmhasan@users.noreply.github.com>
2026-01-17 16:48:37 +0600

    Update page.jsx

--- store/src/app/contact-us/page.jsx
+++ store/src/app/contact-us/page.jsx
@@ -11,8 +11,6 @@ import PageHeader from "@components/header/PageHeader";
 import CMSkeletonTwo from "@components/preloader/CMSkeleton";
 import { getStoreCustomizationSetting } from "@services/SettingServices";
 
-export const dynamic = "force-dynamic";
-
 export const metadata = {
   title: "Contact Us | Kachabazar",
   description:
