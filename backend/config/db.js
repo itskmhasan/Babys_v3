b0de67b8dbe9118b139ef21c7c148e9b75cd2149
Kazi Mahibol Hasan <33973244+itskmhasan@users.noreply.github.com>
2026-01-17 18:59:21 +0600

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      retryWrites: true,
+      w: "majority",
+    });
     console.log("mongodb connection success!");
   } catch (err) {
     console.log("mongodb connection failed!", err.message);
